74259301199bef7c27183e2c1711346d461af32d
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-20 12:31:25 +0300

    Implement tickets' models

--- tickets/models.py
+++ tickets/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from flights.models import Flight
 
-# Create your models here.
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        to=get_user_model(), on_delete=models.CASCADE, related_name="orders"
+    )
+
+
+class Ticket(models.Model):
+    row = models.IntegerField()
+    seat = models.IntegerField()
+    flight = models.ForeignKey(
+        to=Flight, on_delete=models.CASCADE, related_name="tickets"
+    )
+    order = models.ForeignKey(
+        to=Order, on_delete=models.CASCADE, related_name="tickets"
+    )
