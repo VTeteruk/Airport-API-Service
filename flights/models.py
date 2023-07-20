8cb3097821259f21a0aef0aa0089cd72967192f5
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-20 12:39:58 +0300

    Implement flights' models

--- flights/models.py
+++ flights/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+from airplanes.models import Airplane
+from crews.models import Crew
+
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    is_capital = models.BooleanField()
+
+
+class Airport(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.ForeignKey(
+        to=City, on_delete=models.CASCADE, related_name="airports"
+    )
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        to=Airport, on_delete=models.CASCADE, related_name="routes"
+    )
+    destination = models.ForeignKey(
+        to=Airport, on_delete=models.CASCADE, related_name="routes"
+    )
+    distance = models.IntegerField()
+
+
+class Flight(models.Model):
+    crews = models.ManyToManyField(
+        to=Crew, related_name="flights"
+    )
+    route = models.ForeignKey(
+        to=Route, on_delete=models.CASCADE, related_name="flights"
+    )
+    airplane = models.ForeignKey(
+        to=Airplane, on_delete=models.CASCADE, related_name="flights"
+    )
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
