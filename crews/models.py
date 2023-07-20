9675508546f22bbc3c695fe6f76a52738680e6db
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-20 12:23:23 +0300

    Implement crews' models

--- crews/models.py
+++ crews/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    position = models.ForeignKey(
+        to=Position, on_delete=models.CASCADE, related_name="crews"
+    )
