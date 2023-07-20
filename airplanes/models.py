0863f4652bd5b69295ac5b63b69dbdcb9b46baee
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-20 12:22:24 +0300

    Implement airplanes' models

--- airplanes/models.py
+++ airplanes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class AirplaneType(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Airplane(models.Model):
+    name = models.CharField(max_length=255)
+    rows = models.IntegerField()
+    seats_in_row = models.IntegerField()
+    airplane_type = models.ForeignKey(
+        to=AirplaneType, on_delete=models.CASCADE, related_name="airplanes"
+    )
