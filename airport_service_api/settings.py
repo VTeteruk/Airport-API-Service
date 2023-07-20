26e370c6488a65cf822552d7beebb5eec50f4f2d
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-20 12:48:46 +0300

    Make migrations

--- airport_service_api/settings.py
+++ airport_service_api/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "airplanes",
+    "crews",
+    "flights",
+    "tickets",
+    "users",
 ]
 
 MIDDLEWARE = [
