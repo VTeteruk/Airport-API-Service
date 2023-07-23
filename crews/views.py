6aa2dcffda944ec5075ea08c90303a135a5ad7c0
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-23 19:17:08 +0300

    Add filtering for Crews' models

--- crews/views.py
+++ crews/views.py
@@ -1,3 +1,4 @@
+from django.db.models import QuerySet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from crews.models import Position, Crew
@@ -10,8 +11,33 @@ class PositionView(ModelViewSet):
     serializer_class = PositionSerializer
     permission_classes = (IsAdminOrReadOnly, IsAuthenticated)
 
+    def get_queryset(self) -> QuerySet:
+        queryset = self.queryset
+
+        name = self.request.query_params.get("name")
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+
+        return queryset
+
 
 class CrewView(ModelViewSet):
     queryset = Crew.objects.all()
     serializer_class = CrewSerializer
     permission_classes = (IsAdminOrReadOnly, IsAuthenticated)
+
+    def get_queryset(self) -> QuerySet:
+        queryset = self.queryset
+
+        position = self.request.query_params.get("position")
+        first_name = self.request.query_params.get("first_name")
+        last_name = self.request.query_params.get("last_name")
+
+        if position:
+            queryset = queryset.filter(position__name__icontains=position)
+        if first_name:
+            queryset = queryset.filter(first_name__icontains=first_name)
+        if last_name:
+            queryset = queryset.filter(last_name__icontains=last_name)
+
+        return queryset
