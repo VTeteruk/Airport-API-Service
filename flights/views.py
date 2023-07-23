1debaa76dd6a0b468a7692fd02bfd07334c4858a
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-23 19:20:14 +0300

    Fix/importing & add annotations

--- flights/views.py
+++ flights/views.py
@@ -1,7 +1,6 @@
 from django.db.migrations import serializer
-from django.db.models import F, Count
-from rest_framework.permissions import IsAuthenticated, SAFE_METHODS, \
-    IsAdminUser
+from django.db.models import F, Count, QuerySet
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from airport_service_api.permissions import IsAdminOrReadOnly
 from flights.models import City, Airport, Route, Flight
@@ -27,7 +26,7 @@ class AirportView(ModelViewSet):
     queryset = Airport.objects.all()
     permission_classes = (IsAuthenticated, IsAdminOrReadOnly)
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> serializer:
         if self.request.method == "GET":
             return AirportListSerializer
         return AirportSerializer
@@ -49,17 +48,21 @@ class FlightView(ModelViewSet):
     queryset = Flight.objects.all()
     permission_classes = (IsAuthenticated, IsAdminOrReadOnly)
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         queryset = self.queryset
         if self.action == "list":
             queryset = (
                 queryset
                 .select_related("airplane")
-                .annotate(seats_available=F("airplane__rows") * F("airplane__seats_in_row") - Count("tickets"))
+                .annotate(
+                    seats_available=F(
+                        "airplane__rows"
+                    ) * F("airplane__seats_in_row") - Count("tickets")
+                )
             ).order_by("id")
         return queryset
 
-    def get_serializer_class(self):
+    def get_serializer_class(self) -> serializer:
         if self.action == "retrieve":
             return FlightDetailSerializer
         if self.request.method == "GET":
