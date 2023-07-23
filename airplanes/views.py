c88907f32d0e1aba00fe81ebbd38d237fdeb47c9
Vlad Tetruk <vladteteruk01@gmail.com>
2023-07-23 19:06:42 +0300

    Add filtering by name for Airpane's models & Fix/code duplication

--- airplanes/views.py
+++ airplanes/views.py
@@ -1,3 +1,4 @@
+from django.db.models import QuerySet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 from airplanes.models import Airplane, AirplaneType
@@ -5,13 +6,23 @@ from airplanes.serializers import AirplaneSerializer, AirplaneTypeSerializer
 from airport_service_api.permissions import IsAdminOrReadOnly
 
 
-class AirplaneView(ModelViewSet):
+class BaseAirplaneViewSet(ModelViewSet):
+    permission_classes = (IsAdminOrReadOnly, IsAuthenticated)
+
+    def get_queryset(self) -> QuerySet:
+        queryset = self.queryset
+
+        name = self.request.query_params.get("name")
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
+
+
+class AirplaneView(BaseAirplaneViewSet):
     queryset = Airplane.objects.all()
     serializer_class = AirplaneSerializer
-    permission_classes = (IsAdminOrReadOnly, IsAuthenticated)
 
 
-class AirplaneTypeView(ModelViewSet):
+class AirplaneTypeView(BaseAirplaneViewSet):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
-    permission_classes = (IsAdminOrReadOnly, IsAuthenticated)
